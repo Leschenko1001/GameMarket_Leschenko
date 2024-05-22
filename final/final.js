657774f4148e179b025ebe5799a2b9eee8941ac5
Leschenko1001 <158287421+Leschenko1001@users.noreply.github.com>
2024-05-22 14:49:03 +0300

    додала учня

--- final/final.js
+++ final/final.js
@@ -1,100 +1,40 @@
-  // Перевірка підключеного файлу скриптів
-// console.log('Перевірка підключеного файлу скриптів market.js')
-
-//Визначення масиву товарів
-// let itemsArray = [
-//     'Газонокосарка 43',
-//     'Електричний тример 110',
-//     'Електрична газонокосарка 32',
-//     'Акумуляторний оприскувач 12 N',
-//     'Газонокосарка 44',
-//     'Електричний тример 111',
-//     'Електрична газонокосарка 33',
-//     'Акумуляторний оприскувач 13 N',
-//     'Газонокосарка 45',
-//     'Електричний тример 112',
-//     'Електрична газонокосарка 34',
-//     'Акумуляторний оприскувач 14 N',
-//     // 'Газонокосарка 430',
-//     // 'Електричний тример 130',
-//     // 'Електрична газонокосарка 320',
-//     // 'Акумуляторний оприскувач 12 E',
-// ]
+ 
 
 let itemsArray = [
-    {
-        title: "Мотокоса 43",
-        image: "https://hotline.ua/img/tx/319/3193450945.jpg",
-        mono: 43,
-        pb: 86,
-        price_started: "5 499",
-        price_discount: "4 497",
-        price_coupon: "4 395",
-    },
-    {
-       title: "Електричний тример 110",
-       image: "https://listasad.com.ua/images/0zYEp3pYvDHnhgOHNybNOFpXgsh6Iiz2f6J3xDKiNMs/w:600/bG9jYWw/6Ly8vbGlzdGFzYWQuY29tLnVhL3B1YmxpY19odG1sL3N0b3JhZ2UvYXBwL21lZGlhLzI3OTY1L090bWlUSTVyYkFWeTJZYjNDUGE3WUtNWDVid1laNi1tZXRhWTJGMFlXeHZaMTl2Y21sbmFXNWZNamd5TkRFeExtcHdadz09LS5qcGc.webp",
-       mono: 43,
-       pb: 80,
-       price_started: "4 497",
-       price_discount: "3 498",
-       price_coupon: "3 396",
-   },
-   {
-    title: "Електрична газонокосарка 32",
-    image: "https://aqb.com.ua/content/images/34/480x480l50nn0/elektrycheskaia-hazonokosylka-gardena-powermax-32-1200-g2-40502069671976.jpg",
-    mono: 43,
-    pb: 87,
-    price_started: "4 497",
-    price_discount: "3 297",
-    price_coupon: "3 195",
- },
- {
-    title: "Акумуляторний обприскувач 12 N",
-    image: "https://hotline.ua/img/tx/449/4492289865.jpg",
-    mono: 43,
-    pb: 47,
-    price_started: "2 190",
-    price_discount: "1 698",
-    price_coupon: "1 596",
- },
- ]
+    
+        {
+            email: "23b_lam@liceum.ztu.edu.ua",
+            class: "10-Б 2",
+            name: "Лещенко Настя",
+            nickname: "dude настюшкі",
+            authorPhoto: "img1/Лещенко.jpg",
+            project1_name: "Phaser2ndGame1",
+            project1_githubRepo: "https://github.com/Leschenko1001/Phaser2ndGame1?authuser=0",
+            project1_liveDemo: "https://leschenko1001.github.io/Phaser2ndGame1/?authuser=0",
+            authorPhoto1: "",
+            project2_name: "GameMarket_Leschenko",
+            project2_githubRepo: "https://github.com/Leschenko1001/GameMarket_Leschenko",
+            project2_liveDemo: "https://leschenko1001.github.io/GameMarket_Leschenko/?authuser=0",
+            gamePhoto: "",
+            googleDriveFolder: "https://drive.google.com/drive/folders/1CqBXUS5Rk1u8nhEq3_l7LYMjDalYqPgW?usp=classroom_web&authuser=0",
+            googleForm: "https://forms.gle/aLmXJ7nEnjo6PGuZ8?authuser=0",
+            marketDemo: "https://leschenko1001.github.io/GameMarket_Leschenko/market.html?authuser=0",
+            finalDemo: "https://leschenko1001.github.io/GameMarket_Leschenko/final/final.html?authuser=0"
+          }
+          
+    
+]
  
  // Отримання елементу з ідентифікатором items
  let itemsDiv = document.getElementById("items");
  
  // Перевірка існування знайденого блоку
  if (itemsDiv) {
-    // Вивід знайденого елементу
-    // console.log(itemsDiv)
- 
-    // // Вивід значення поля знайденого елементу
-    // console.log('Поле classList: ', itemsDiv.classList)
-    // console.log('Поле id: ', itemsDiv.id)
-    // console.log('Поле clientWidth: ', itemsDiv.clientWidth)
-    // console.log('Поле innerHTML: ', itemsDiv.innerHTML)
- 
-    // // Додавання тексту в блок
-    // itemsDiv.innerText = 'Перший Програмно доданий текст'
-    // // itemsDiv.innerText = 'Другий Програмно доданий текст' 
-    // itemsDiv.innerText += 'Другий Програмно доданий текст'
- 
-    // Додавання відформатованого HTML коду в блок
-    // itemsDiv.innerHTML = '<div class = item></div>'
-    // itemsDiv.innerHTML += '<div class = item></div>'
-    // itemsDiv.innerHTML += '<div class = item></div>'
-    // itemsDiv.innerHTML += '<div class = item></div>'
-    // itemsDiv.innerHTML += '<div class = item></div>'
- 
-    // for (let i = 0; i < 100; i++) {
-    //     itemsDiv.innerHTML += '<div class = item></div>'
-    // }
+   
     //Виведення елементів масиву
     itemsArray
     .forEach((item, index) => {
-        // console.log(item)
-        // itemsDiv.innerText += item
-        // itemsDiv.innerHTML += `<div class = "item">${item}</div>`
+        
         //Виводимо на веб сторінку елемент масиву в блок з класом item
         itemsDiv.innerHTML += 
         `
@@ -103,6 +43,7 @@ let itemsArray = [
         <div class="item-title">${item.title}</div>
         <!-- Блок зображення товару -->
         <div class="item-image"> 
+            <img1 src="img1/${item.authorPhoto}" class="item-image">
             <img src="${item.image}" class="item-image">
         </div>
         <!-- Блок з оплатою частинами -->
@@ -129,41 +70,4 @@ let itemsArray = [
     console.log('Блок товарів не знайдено')
  }
  
- // //Визначення масиву товарів
- // // let itemsArray = ['','','','']
- // let itemsArray = [
- //     'Газонокосарка 43',
- //     'Електричний тример 110',
- //     'Електрична газонокосарка 32',
- //     'Акумуляторний оприскувач 12 N',
- //     // 'Газонокосарка 430',
- //     // 'Електричний тример 130',
- //     // 'Електрична газонокосарка 320',
- //     // 'Акумуляторний оприскувач 12 E',
- // ]
  
- // //Виведення елементів масиву
- // itemsArray.forEach((item) => {
- //     console.log(item)
- // })
- 
- // //Виведення елементів відсортованого масиву
- // itemsArray.sort().forEach((item) => {
- //     console.log(index + '-й елемент', item)
- // })
- 
- // //Виведення в консоль масиву
- // console.log(itemsArray)
- 
- // //Виведення в консоль елементів масиву
- // for (let i=0; i<itemsArray.length; i++) {
- //     console.log(itemsArray[i])
- // }
- 
- // //Сортування масиву
- // itemsArray = itemsArray.sort()
- 
- // //Виведення в консоль номерів та значень елементів масиву
- // for (let i=0; i<itemsArray.length; i++) {
- //     console.log(i + '-й елемент: ', itemsArray[i])
- // }
